5a896f812a80b59ad5dd573d12454f9bce72af4a
mcflurry0718 <138760056+mcflurry0718@users.noreply.github.com>
2023-07-06 08:51:22 +0100

    Update GameController.java

--- GameController.java
+++ GameController.java
@@ -1,5 +1,3 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,50 +5,81 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * REST controller for managing games.
+ */
 @RestController
 @RequestMapping("/games")
 public class GameController {
 
-    private static final Logger log = LoggerFactory.getLogger(GameController.class);
-
     @Autowired
     private GameService gameService;
 
-    @GetMapping("/")
+    /**
+     * Returns a list of all games.
+     *
+     * @return a list of all games
+     */
+    @GetMapping
     public List<Game> getAllGames() {
-        log.info("Retrieving all games");
         return gameService.getAllGames();
     }
 
+    /**
+     * Returns the game with the specified name.
+     *
+     * @param name the name of the game to retrieve
+     * @return the game with the specified name
+     * @throws GameNotFoundException if no game with the specified name is found
+     */
     @GetMapping("/{name}")
-    public ResponseEntity<Game> getGameByName(@PathVariable String name) {
-        log.info("Retrieving game with name: {}", name);
-        try {
-            Game game = gameService.getGameByName(name);
-            return ResponseEntity.ok(game);
-        } catch (GameNotFoundException e) {
-            return ResponseEntity.notFound().build();
-        }
+    public Game getGameByName(@PathVariable String name) throws GameNotFoundException {
+        return gameService.getGameByName(name);
     }
 
-    @PostMapping("/")
-    public ResponseEntity<Void> createGame(@RequestBody Game game) {
-        log.info("Creating game: {}", game);
-        gameService.createGame(game);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    /**
+     * Creates a new game.
+     *
+     * @param game the game to create
+     * @return the created game
+     */
+    @PostMapping
+    public ResponseEntity<Game> createGame(@RequestBody Game game) {
+        Game createdGame = gameService.createGame(game);
+        return new ResponseEntity<>(createdGame, HttpStatus.CREATED);
     }
 
-    @PutMapping("/")
-    public ResponseEntity<Void> updateGame(@RequestBody Game game) {
-        log.info("Updating game: {}", game);
-        gameService.updateGame(game);
-        return ResponseEntity.ok().build();
+    /**
+     * Updates an existing game.
+     *
+     * @param game the game to update
+     * @return the updated game
+     * @throws GameNotFoundException if no game with the specified name is found
+     */
+    @PutMapping("/{name}")
+    public Game updateGame(@PathVariable String name, @RequestBody Game game) throws GameNotFoundException {
+        return gameService.updateGame(name, game);
     }
 
+    /**
+     * Deletes the game with the specified name.
+     *
+     * @param name the name of the game to delete
+     * @return an empty response with HTTP status 204 (No Content)
+     * @throws GameNotFoundException if no game with the specified name is found
+     */
     @DeleteMapping("/{name}")
-    public ResponseEntity<Void> deleteGame(@PathVariable String name) {
-        log.info("Deleting game with name: {}", name);
+    public ResponseEntity<Void> deleteGame(@PathVariable String name) throws GameNotFoundException {
         gameService.deleteGame(name);
         return ResponseEntity.noContent().build();
+    }
+
+    /**
+     * Sets the game service to use for managing games.
+     *
+     * @param gameService the game service to use
+     */
+    public void setGameService(GameService gameService) {
+        this.gameService = gameService;
     }
 }
