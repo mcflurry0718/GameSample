2b181554c5f525f5b3d940d1a94b0b21347748a4
mcflurry0718 <138760056+mcflurry0718@users.noreply.github.com>
2023-07-06 10:15:53 +0100

    Update GameNotFoundException.java

--- src/main/java/game/sample/exception/GameNotFoundException.java
+++ src/main/java/game/sample/exception/GameNotFoundException.java
@@ -1,6 +1,15 @@
 package game.sample.exception;
 
+/**
+ * Exception thrown when a requested game is not found.
+ */
 public class GameNotFoundException extends RuntimeException {
+    
+    /**
+     * Constructs a new GameNotFoundException with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public GameNotFoundException(String message) {
         super(message);
     }
